dbc04b5dae7335bb9aa7d8660caed56834c90f85
Amit <heroku467@gmail.com>
2025-03-22 21:11:09 +0530

    Update Fsub.py

--- plugins/Fsub.py
+++ plugins/Fsub.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
-from config import AUTH_CHANNEL
+from config import AUTH_CHANNEL, FORCE_PIC
 
 #=====================================================================================
 
