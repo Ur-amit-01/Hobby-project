333744a677b06f8d4c3d8d97b535fa73cf34bd34
Amit <heroku467@gmail.com>
2025-03-29 19:50:25 +0530

    Update animated.py

--- plugins/animated.py
+++ plugins/animated.py
@@ -1,7 +1,5 @@
 from pyrogram import Client, filters
 
-app = Client("my_bot", api_id=API_ID, api_hash="API_HASH", bot_token="BOT_TOKEN")
-
 CUSTOM_EMOJI_ID = "5472055112702629499"  # Your premium emoji ID
 
 @Client.on_message(filters.command("emoji"))
