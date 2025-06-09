2e748bcf199c3ec9009bec3a24e233c5e7ec476d
Amit <heroku467@gmail.com>
2025-06-10 00:24:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 DB_NAME = os.environ.get("DB_NAME","Merger")     
 DB_URL = os.environ.get("DB_URL","mongodb+srv://uramit0001:EZ1u5bfKYZ52XeGT@cluster0.qnbzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6803505727 7150972327').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '2031106491').split()]
 PORT = os.environ.get("PORT", "8080")
 
 RENAME_MODE = bool(environ.get('RENAME_MODE', True)) # Set True or False
@@ -20,7 +20,7 @@ SESSION_STRING = os.environ.get("SESSION_STRING", "")
 
 
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/nrg.jpg")
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002385466192').split()] #Ex : ('-10073828 -102782829 -1007282828')
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '').split()] #Ex : ('-10073828 -102782829 -1007282828')
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002027394591"))
 
 REACTIONS = ["🤝", "😇", "🤗", "😍", "🎅", "🥰", "🤩", "😘", "😛", "😈", "🎉", "🫡", "😎", "🔥", "🤭", "🌚", "🆒", "👻", "😁"] #don't add any emoji because tg not support all emoji reactions
